30e311f44fb76fff1cc3f80cbc4a044e3f8d2013
htetas <htetaks@gmail.com>
2025-01-18 09:19:35 +0700

    added pid and message for client

--- src/client.c
+++ src/client.c
@@ -1,17 +1,19 @@
 #include "../include/minitalk.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-void	main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	int	pid;
 	char	*str;
 
 	if (ac != 3)
-		printf("Please input Server Pid and one message"\n);
+		printf("Please input Server Pid and one message\n");
 	else
 	{
-		pid = ft_atoi(av[1]);
+		pid = atoi(av[1]);
 		str = av[2];
+		printf("Server ID is %d and the message is %s\n", pid, str);
 		//to add code to send message
 	}
 	return (0);
